1545c27a92ed8e4ac3a265eb073b5b64ef297c40
Shaan Menon <smenon18@gmail.com>
2022-12-01 17:07:49 -0500

    Day 1 Solution python

---
+++ day_one.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    """"
+    First Problem for Advent Of Code 2022
+    :return: none
+    """
+    file = open('./input/dayOne.txt', "r")
+    elves = [[int(line) for line in block.split("\n")] for block in file.read()[:-1].split("\n\n")]
+    file.close()
+    print("Part 1: ")  # find highest cal count
+    print(max(sum(e) for e in elves))
+    print("Part 2: ")  # find sum of 3 highest cal count
+    print(sum(sorted([sum(e) for e in elves])[-3:]))
+
+
+if __name__ == '__main__':
+    main()
