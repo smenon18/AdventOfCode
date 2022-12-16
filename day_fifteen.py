325f200e07f6066ad9a2327f17d0b8b209f2aa0b
Shaan Menon <smenon18@gmail.com>
2022-12-16 12:09:55 -0500

    add typing, fix some comments day 15

--- day_fifteen.py
+++ day_fifteen.py
@@ -11,9 +11,9 @@ def main() -> None:
     """
     file = open('./input/dayFifteen.txt', 'r')
     limit = 2000000
-    beacons = set()  # keep set of beacons and signals to remove from other set at end
-    beaconless = set()
-    sensors = {}
+    beacons: set[int] = set()  # keep set of beacons and signals to remove from other set at end
+    beaconless: set[int] = set()
+    sensors: dict[tuple[int, int], int] = {}
     for line in file.read().split('\n'):
         sx, sy = int(line[12: line.index(',')]), int(line[line.index('y') + 2: line.index(':')])
         bx, by = int(line[line.rindex('x') + 2: line.rindex(',')]), int(line[line.rindex('y') + 2:])
@@ -29,7 +29,7 @@ def main() -> None:
     print(len(beaconless))
     max_xy = 4000000
     for s_point, dist in sensors.items():
-        sx, sy = s_point  # create ranges with this that mimicks the data passed into set union from p1
+        sx, sy = s_point  # create ranges with this that mimics the data passed into set union from p1
         for i in range(max(0, sx-dist-1), min(max_xy, sx + dist + 2)):
             for j in [sy + (dist + 1 - abs(i - sx)), sy - (dist + 1 - abs(i - sx))]:
                 if j in range(max_xy + 1):
@@ -39,7 +39,7 @@ def main() -> None:
                     else:
                         print('Part B:')
                         print(i * max_xy + j)
-                        return  # would be better to make this look function and return printed line above
+                        return  # would be better to make this loop seq a function and return printed line above
 
 
 if __name__ == "__main__":
