088fe21d1f5e46105e2df292863c949fac5d877e
Shaan Menon <smenon18@gmail.com>
2022-12-16 16:14:21 -0500

    finally pt 2, would be so much easier and more readable with objects

--- day_sixteen.py
+++ day_sixteen.py
@@ -35,26 +35,44 @@ def bfs(valve_name: str, mins_left: int, valves, part2=False):
             if other is not v and other in flowing_valves:
                 dist_to_others[v][other] = calc_mins(v, other, valves)
     max_flow = 0
-    if not part2:
-        valve_states = [(mins_left, valve_name, [], 0, 0)]
-        while len(valve_states) > 0:
-            curr_state = valve_states.pop()
-            max_flow = max(max_flow, get_flow(curr_state))
-            valve = valves[curr_state[1]]
-            if curr_state[0] == 0:
+    state_best = {}
+    state_all = {}
+    valve_states = [(mins_left, valve_name, [], 0, 0)]
+    while len(valve_states) > 0:
+        curr_state = valve_states.pop()
+        max_flow = max(max_flow, get_flow(curr_state))
+        valve = valves[curr_state[1]]
+        if curr_state[0] == 0:
+            continue
+        if curr_state[1] not in curr_state[2] and valve['flow_rate'] > 0:
+            curr_state = (curr_state[0] - 1, curr_state[1], curr_state[2][:] + [curr_state[1]],
+                          curr_state[3] + valve['flow_rate'], curr_state[4] + curr_state[3])
+            valve_states.append(curr_state)
+            if part2:
+                all_opened = sorted(curr_state[2])
+                all_opened = tuple(all_opened)
+                if state_best.get(all_opened, 0) < get_flow(curr_state):
+                    state_best[all_opened] = get_flow(curr_state)
+                    state_all[all_opened] = curr_state
+            continue
+        for allowed_dest in flowing_valves:
+            if allowed_dest in curr_state[2]:
                 continue
-            if curr_state[1] not in curr_state[2] and valve['flow_rate'] > 0:
-                valve_states.append((curr_state[0]-1, curr_state[1], curr_state[2][:] + [curr_state[1]], curr_state[3] + valve['flow_rate'], curr_state[4] + curr_state[3]))
-                continue
-            for allowed_dest in flowing_valves:
-                if allowed_dest in curr_state[2]:
-                    continue
-                dist_to_dest = dist_to_others[curr_state[1]][allowed_dest]
-                if (curr_state[0] - dist_to_dest) > 1:
-                    valve_states.append((curr_state[0] - dist_to_dest, allowed_dest, curr_state[2],
-                                         curr_state[3], curr_state[4] + (curr_state[3] * dist_to_dest)))
-    else:
-        print('')
+            dist_to_dest = dist_to_others[curr_state[1]][allowed_dest]
+            if (curr_state[0] - dist_to_dest) > 1:
+                valve_states.append((curr_state[0] - dist_to_dest, allowed_dest, curr_state[2],
+                                     curr_state[3], curr_state[4] + (curr_state[3] * dist_to_dest)))
+    if part2:
+        max_flow = 0
+        for you in state_all.values():
+            for elephant in state_all.values():
+                state_valid = True
+                for v in you[2]:
+                    if v in elephant[2]:
+                        state_valid = False
+                        break
+                if state_valid:
+                    max_flow = max(max_flow, get_flow(you) + get_flow(elephant))
     return max_flow
 
 
