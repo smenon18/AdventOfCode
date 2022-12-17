22a1b78975f7251650738a9806f66bc18ab96bd7
Shaan Menon <smenon18@gmail.com>
2022-12-17 00:41:38 -0500

    started day 17

---
+++ day_seventeen.py
@@ -0,0 +1,20 @@
+
+
+def main() -> None:
+    """
+    Day Seventeen of Advent of Code 2022
+    :return: None
+    """
+    file = open('./input/daySeventeen.txt', 'r')
+    push_seq = list(file.read())
+    file.close()
+    width = 7  # width is seven spawn 3 from left
+    h = 3  # curr height is lowest block + 3 / floor + 3 for no blocks
+    print(len(push_seq))
+    j = 0  # jet counter
+    for i in range(2022):
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
