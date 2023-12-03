3646fe438a978a9bb2178ad94da8cfc67b4849dd
Shaan Menon <smenon18@gmail.com>
2023-12-03 12:39:01 -0500

    Advent of Code day 3 code clean up

--- 2023/day_three.py
+++ 2023/day_three.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 
 SYMBOL_REGEX = re.compile('[@_!#$%^&*()<>?/\|}{~:+=`~-]')
-NUMS_REGEX = '\d+'
+NUMS_REGEX = re.compile('\d+')
 
 
 def find_symbol(data, min_x, min_y, max_x, max_y) -> bool:
@@ -22,9 +22,11 @@ def main() -> None:
     file = open('input/dayThree.txt', 'r')
     data = [line.strip() for line in file]
     file.close()
+    gears = defaultdict(list)
     sum_part_nums = 0
     for idx, line in enumerate(data):
-        for match in re.finditer(NUMS_REGEX, line):
+        for match in NUMS_REGEX.finditer(line):
+            num = int(match.group())
             min_x = max(0, idx-1)
             min_y = max(0, match.start()-1)
             max_x = min(len(data)-1, idx+1)
@@ -32,20 +34,12 @@ def main() -> None:
             found = find_symbol(data, min_x, min_y, max_x, max_y)
             if found:
                 sum_part_nums += int(match.group())
-    print('Part 1:', sum_part_nums)
-    gears = defaultdict(list)
-    for idx, line in enumerate(data):
-        for match in re.finditer(NUMS_REGEX, line):
-            curr = int(match.group())
-            min_x = max(idx-1, 0)
-            max_x = min(idx+1, len(data)-1)
-            min_y = max(0, match.start() - 1)
-            max_y = min(len(line)-1, match.end())
             for x in range(min_x, max_x + 1):
-                for y in range(min_y, max_y + 1):
+                for y in range(min_y, max_y):
                     c = data[x][y]
                     if c == '*':
-                        gears[(x, y)].append(curr)
+                        gears[(x, y)].append(num)
+    print('Part 1:', sum_part_nums)
     sum_gear_ratio = 0
     for nums in gears.values():
         if len(nums) == 2:
